f3b8d1aad133722c3cfcdc1c09919a2e38a37c0a
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-08 18:12:28 -0700

    completed most of model

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -17,8 +17,8 @@ function find() { // EXERCISE A
     2A- When you have a grasp on the query go ahead and build it in Knex.
     Return from this function the resulting dataset.
   */
- return db('schemes as sc')
-    .leftJoin('stpes as st', 'sc.scheme_id', 'st.scheme_id')
+    return db('schemes as sc')
+    .innerJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
     .select('sc.*')
     .count('st.step_id as number_of_steps')
     .groupBy('sc.scheme_id')
@@ -91,25 +91,25 @@ async function findById(scheme_id) { // EXERCISE B
       }
   */
       const rows = await db('schemes as sc')
-      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
-      .where('sc.scheme_id', scheme_id)
-      .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
-      .orderBy('st.step_number')
-    const result = {
-      scheme_id: rows[0].scheme_id,
-      scheme_name: rows[0].scheme_name,
-      steps:[]
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .where('sc.scheme_id', scheme_id)
+    .select('st.*', 'sc.scheme_name', 'sc.scheme_id')
+    .orderBy('st.step_number')
+  const result = {
+    scheme_id: rows[0].scheme_id,
+    scheme_name: rows[0].scheme_name,
+    steps:[]
+  }
+  rows.forEach(row => {
+    if (row.step_id) {
+      result.steps.push({
+        step_id: row.step_id,
+        step_number: row.step_number,
+        instructions: row.instructions,
+      })
     }
-    rows.forEach(row => {
-      if (row.step_id) {
-        result.steps.push({
-          step_id: row.step_id,
-          step_number: row.step_number,
-          instructions: row.instructions,
-        })
-      }
-    })
-    return result
+  })
+  return result
 }
 
 async function findSteps(scheme_id) { // EXERCISE C
@@ -139,9 +139,9 @@ async function findSteps(scheme_id) { // EXERCISE C
       .where('sc.scheme_id', scheme_id)
       .orderBy('step_number')
     if (!rows[0].step_id) {
-      return []
-    }
-    return rows
+    return []
+  }
+  return rows
 }
 
 function add(scheme) { // EXERCISE D
